76635aae521e55189dd73be161a5962d13e24ecd
Kayeen Campaña <159712236+Topor24k@users.noreply.github.com>
2026-03-11 17:53:26 +0800

    Update LoginRequest validation

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -6,6 +6,13 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
+/**
+ * PHPDoc annotation for IDE support
+ * Documents validated request properties for static analysis
+ * 
+ * @property string $email User's email address from validated request data
+ * @property string $password User's password from validated request data
+ */
 class LoginRequest extends FormRequest
 {
     /**
